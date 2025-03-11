ea5779fffa812a5ac956fb5a715ea1c63600f8ea
Y22wer <teddy857zxc@gmail.com>
2025-03-11 16:19:30 +0800

    部屬條參數

--- to_backend/to_backend/settings.py
+++ to_backend/to_backend/settings.py
@@ -79,13 +79,16 @@ WSGI_APPLICATION = 'to_backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
